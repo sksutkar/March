92ebd855f9aba499592e8b1ee282ae76fd3fbdea
sksutkar <swapnilsutkar7123@gmail.com>
2026-03-28 11:45:37 +0530

    march28 updated...

--- march28.js
+++ march28.js
@@ -2,9 +2,23 @@
 
 // Welcome To Target Goals... In 2026
 
-console.log("Welcome To Target Goals...");
+// console.log("Welcome To Target Goals...");
 
-console.log("Swapnil_712");
+// console.log("Swapnil_712");
+
+// Reverse a Number
+
+let Num = 712;
+let Res = 0;
+
+while (Num > 0) {
+    let R = Num % 10;
+    Res = Res * 10 + R;
+    Num = Math.floor(Num / 10);
+
+}
+
+console.log("The Reverse Number", Res);
 
 
 
